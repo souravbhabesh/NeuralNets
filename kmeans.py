bfbc24f10a620b07a625a19c70cdae9a04541618
souravbhabesh <souraviitm@gmail.com>
2019-10-28 01:21:09 -0400

    WIP: Dunn index implemented

--- kmeans.py
+++ kmeans.py
@@ -3,7 +3,21 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-def kmeans(data, k, iteration = 10):
+
+def dunnIndex(min_inter_cluster_distance, max_intra_cluster_distance):
+    return min_inter_cluster_distance / max_intra_cluster_distance
+
+def interClusterDistance(centroids):
+    num_centroids = centroids.shape[0]
+    min_inter_cluster_dist = 0
+    for _c in range(num_centroids-1):
+        for _cnext in range(_c+1,num_centroids):
+            if np.linalg.norm(centroids[_c] - centroids[_cnext]) > min_inter_cluster_dist:
+                min_inter_cluster_dist = np.linalg.norm(centroids[_c] - centroids[_cnext])
+    return min_inter_cluster_dist
+
+
+def kmeans(data, k, iteration=10):
     # Select k random points from the data as centroids
     k_random_pts = np.random.randint(data.shape[0], size=k)
     centroid = [data[i] for i in k_random_pts]
@@ -21,18 +35,25 @@ def kmeans(data, k, iteration = 10):
         cluster_assignment = np.argmin(intra_cluster_dist, axis=1)
         # print(cluster_assignment.shape)
 
+        # Calculating Dunn Index for current cluster assignments
+        max_intra_cluster_distance = np.max(intra_cluster_dist)
+        min_inter_cluster_distance = interClusterDistance(np.asarray(centroid))
+        print("Dunn index at iter {0} : {1}".format(_iter,
+                            dunnIndex(min_inter_cluster_distance, max_intra_cluster_distance)))
+
         # Step 4: Recompute the centroids of newly formed clusters
         clusters, cluster_counts = np.unique(cluster_assignment, return_inverse=False, return_counts=True)
         new_centroid = []
         for _col in range(x.shape[1]):
-            new_centroid.append(np.bincount(cluster_assignment, weights=data[:, _col])/cluster_counts)
+            new_centroid.append(np.bincount(cluster_assignment, weights=data[:, _col]) / cluster_counts)
         new_centroid = np.asarray(new_centroid)
         new_centroid = new_centroid.transpose()
         for _k in range(k):
             centroid[_k] = new_centroid[_k]
-        print("Updated centroids are {0} ".format(centroid))
+        # print("Updated centroids are {0} ".format(centroid))
 
     return cluster_assignment, centroid
+
 
 if __name__ == "__main__":
     data = pd.read_csv('clustering.csv')
@@ -44,9 +65,9 @@ if __name__ == "__main__":
     print(x.shape)
     # print(x[:,0])
 
-    cluster_assignment, centroid = kmeans(x, k=3)
+    cluster_assignment, centroid = kmeans(x, k=3, iteration=20)
     plt.scatter(X["ApplicantIncome"], X["LoanAmount"], c=cluster_assignment)
-    plt.scatter(np.asarray(centroid)[:,1], np.asarray(centroid)[:,0], c='red')
+    plt.scatter(np.asarray(centroid)[:, 1], np.asarray(centroid)[:, 0], c='red')
     plt.xlabel('AnnualIncome')
     plt.ylabel('Loan Amount (In Thousands)')
     plt.show()
